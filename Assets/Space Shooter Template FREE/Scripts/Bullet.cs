9328ba734d546e2966d20e0a7652ad80abff7b3e
namdao <namdao261205@gmail.com>
2026-02-08 17:12:22 +0700

    Initial commit

---
+++ Assets/Space Shooter Template FREE/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float flySpeed;
+    public int damage;
+...
+private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var enemy = collision.GetComponent<EnemyHealth>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+...
